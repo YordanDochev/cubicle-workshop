f950ab098853a216e4da9c7d0d2f9bd1fa85d120
YordanDochev <yordan.dochev13@gmail.com>
2023-09-30 16:06:12 +0300

    final commit

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -3,15 +3,15 @@ const uniqid = require('uniqid')
 const cubes = [{
     id: 1,
     name: 'Rubic Cube',
-    description: 'test',
+    description: 'Traditional rubic cube',
     imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBrxH1AoIHNcK-6NkJCYMB_YTdUaL7Dm_QcadSnGTd6hKXt8njiQbe2eSUpQ&s',
     difficultyLevel: '3'
   },
   {
     id: 2,
     name: 'Mirror Cube',
-    description: 'test1',
-    imageUrl: 'https://m.media-amazon.com/images/I/71TrvUl50OL.__AC_SX300_SY300_QL70_FMwebp_.jpg&s',
+    description: 'Very famous mirror cube',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjR578qrrPUT7ihgEEcTsGEIAGE5JpKiwpR8Ml14mFfZ2eoNESF-RHEMrCgQ&s',
     difficultyLevel: '5'
   }
 ]
