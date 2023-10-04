0cc42bce774664f7ad17d28ec6c4b77bd9aaa89a
YordanDochev <yordan.dochev13@gmail.com>
2023-10-04 16:18:28 +0300

    Setup mongoose

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
-const express = require('express');
-const {expressConfig} = require('./config/expressConfig')
-const handlebarsConfig = require('./config/handlebarsConfig')
-const routes = require('./route')
+const express = require("express");
+const { expressConfig } = require("./config/expressConfig");
+const handlebarsConfig = require("./config/handlebarsConfig");
+const dbConnect = require("./config/dbConfig");
+const routes = require("./route");
 
 const app = express();
 
 const PORT = 5000;
 
 //ExpressConfig
-expressConfig(app)
+expressConfig(app);
 
 //HandlebarsConfig
-handlebarsConfig(app)
+handlebarsConfig(app);
+
+//Db Connect
+
+dbConnect()
+    .then(()=>console.log('Db connected sucessfuly'))
+    .catch(err=>{
+        console.log('Erorr:',err);
+    })
 
 //Router
 
-app.use(routes)
+app.use(routes);
 
-app.listen(PORT,()=>console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
