88821cc30e456b77a5f51330738f668cf4562752
YordanDochev <yordan.dochev13@gmail.com>
2023-10-04 16:52:51 +0300

    Delete the hardcoded cubes

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,21 +1,5 @@
 const Cube = require('../model/Cube')
 
-const cubes = [{
-    id: 1,
-    name: 'Rubic Cube',
-    description: 'Traditional rubic cube',
-    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBrxH1AoIHNcK-6NkJCYMB_YTdUaL7Dm_QcadSnGTd6hKXt8njiQbe2eSUpQ&s',
-    difficultyLevel: '3'
-  },
-  {
-    id: 2,
-    name: 'Mirror Cube',
-    description: 'Very famous mirror cube',
-    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjR578qrrPUT7ihgEEcTsGEIAGE5JpKiwpR8Ml14mFfZ2eoNESF-RHEMrCgQ&s',
-    difficultyLevel: '5'
-  }
-]
-
 const getAll = async (search, from, to) => {
   const cubes = await Cube.find().lean();
   
