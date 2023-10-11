46f9c00610f69438e9c8c19770f29e04a428e25f
YordanDochev <yordan.dochev13@gmail.com>
2023-10-11 19:03:16 +0300

    User login

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,3 +1,20 @@
 const User = require('../model/User')
+const bcrypt = require('bcrypt')
 
 exports.register = (userData) => User.create(userData)
+
+exports.login = async (username,password) => {
+    const user = await User.findOne({username});
+
+    if(!user){
+        throw new Error ('Do not have register user with this username or password')
+    }
+
+    const isValid = await bcrypt.compare(password,user.password)
+
+    if(!isValid){
+        throw new Error ('Do not have register user with this username or password')
+    }
+
+    return user;
+}
